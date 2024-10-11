58a3ebeddaceb61a45c0c1c2a682cf6d654020cb
lucasnwvalladares <lucasnwvalladares@users.noreply.github.com>
2024-10-11 23:59:14 +0000

    deploy: f7df5c2037a72ef75c8be562accf4e0e498e2c8f

---
+++ _nuxt/static/1728691150/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmy-website\u002F_nuxt\u002Fstatic\u002F1728691150",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002Fmy-website\u002F",assetsPath:"\u002Fmy-website\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Fcontact"));
